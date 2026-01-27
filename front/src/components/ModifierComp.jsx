c1776cb58224034fe213dcbce9f8a13d9939ac36
Julie TENOU <julie.tenou@epitech.eu>
2026-01-27 19:50:31 +0000

    fix: fix competences update function

--- front/src/components/ModifierComp.jsx
+++ front/src/components/ModifierComp.jsx
@@ -15,7 +15,7 @@ import { useTheme } from "@mui/material/styles";
 
 function ModifierTitle(props) {
   const { children, onClose, ...other } = props;
-  
+
   return (
     <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
       {children}
@@ -43,15 +43,15 @@ ModifierTitle.propTypes = {
 };
 
 export default function ModifierDialogs(data) {
-    const theme = useTheme();
+  const theme = useTheme();
 
-  const {state: currentUser, setState: setCurrentUser, loading} = useContext(ConnexionContext);
+  const { state: currentUser, setState: setCurrentUser, loading } = useContext(ConnexionContext);
 
-    console.log(data);
-    const character = data.character;
-    const left = data.left;
-    const name = data.name;
-    const dataToUpdate = data.dataToUpdate;
+  console.log(data);
+  const character = data.character;
+  const left = data.left;
+  const name = data.name;
+  const dataToUpdate = data.dataToUpdate;
   const [open, setOpen] = React.useState(false);
   const [text, setText] = useState("");
   let newDatas = {};
@@ -66,357 +66,357 @@ export default function ModifierDialogs(data) {
   console.log("character.character", character.character)
 
   const handleSave = () => {
-    if(name === 'force'){
+    if (name === 'force') {
       newDatas = {
-      Force_character: text,
+        Force_character: text,
+      };
+    } else if (name === 'dexte') {
+      newDatas = {
+        Dexte_character: text,
+      };
+    } else if (name === 'resistance') {
+      newDatas = {
+        Resistance_character: text,
+      };
+    } else if (name === 'resilience') {
+      newDatas = {
+        Resilience_character: text,
+      };
+    } else if (name === 'intell') {
+      newDatas = {
+        Intell_character: text,
+      };
+    } else if (name === 'charsime') {
+      newDatas = {
+        Charisme_character: text,
+      };
+    } else if (name === 'bien') {
+      newDatas = {
+        Bien_character: text,
+      };
+    } else if (name === 'mal') {
+      newDatas = {
+        Mal_character: text,
+      };
+    } else if (name === 'instinct') {
+      newDatas = {
+        Instinct_character: text,
+      };
+    } else if (name === 'survie') {
+      newDatas = {
+        Survie_character: text,
+      };
+    } else if (name === 'démonique') {
+      newDatas = {
+        Demonique_character: text,
+      };
+    } else if (name === 'draconique') {
+      newDatas = {
+        Draconique_character: text,
+      };
+    } else if (name === 'xalytien') {
+      newDatas = {
+        Xalytien_character: text,
+      };
+    } else if (name === 'xento') {
+      newDatas = {
+        Xento_character: text,
+      };
+    } else if (name === 'zenolm') {
+      newDatas = {
+        Zenolm_character: text,
+      };
+    } else if (name === 'justiccel') {
+      newDatas = {
+        Justiccel_character: text,
+      };
+    } else if (name === 'cerebrov') {
+      newDatas = {
+        Cerebrov_character: text,
+      };
+    } else if (name === 'xaArch') {
+      newDatas = {
+        XalytienArchaique_character: text,
+      };
+    } else if (name === 'xenArch') {
+      newDatas = {
+        XentoArchaique_character: text,
+      };
+    } else if (name === 'zenArch') {
+      newDatas = {
+        ZenolmArchaique_character: text,
+      };
+    } else if (name === 'justiArch') {
+      newDatas = {
+        JusticcelArchaique_character: text,
+      };
+    } else if (name === 'xaAnt') {
+      newDatas = {
+        XalytienAntique_character: text,
+      };
+    } else if (name === 'd4q') {
+      newDatas = {
+        XentoAntique_character: text,
+      };
+    } else if (name === 'xenAnt') {
+      newDatas = {
+        XentoAntique_character: text,
+      };
+    } else if (name === 'zenAnt') {
+      newDatas = {
+        ZenolmAntique_character: text,
+      };
+    } else if (name === 'justiAnt') {
+      newDatas = {
+        JusticcelAntique_character: text,
+      };
+    } else if (name === 'xaDem') {
+      newDatas = {
+        XalytienDemonique_character: text,
+      };
+    } else if (name === 'xenDem') {
+      newDatas = {
+        XentoDemonique_character: text,
+      };
+    } else if (name === 'ZenDem') {
+      newDatas = {
+        ZenolmDemonique_character: text,
+      };
+    } else if (name === 'JustiDem') {
+      newDatas = {
+        JusticcelDemonique_character: text,
+      };
+    } else if (name === 'zombik') {
+      newDatas = {
+        Zombik_character: text,
+      };
+    } else if (name === 'faerik') {
+      newDatas = {
+        Faerik_character: text,
+      };
+    } else if (name === 'elfik') {
+      newDatas = {
+        Elfik_character: text,
+      };
+    } else if (name === 'nanien') {
+      newDatas = {
+        Nanien_character: text,
+      };
+    } else if (name === 'gnomik') {
+      newDatas = {
+        Gnomik_character: text,
+      };
+    } else if (name === 'spectrale') {
+      newDatas = {
+        Spectrale_character: text,
+      };
+    } else if (name === 'astrale') {
+      newDatas = {
+        Astrale_character: text,
+      };
+    } else if (name === 'tenebriale') {
+      newDatas = {
+        Tenebriale_character: text,
+      };
+    } else if (name === 'noyale') {
+      newDatas = {
+        Noyale_character: text,
+      };
+    } else if (name === 'elementale') {
+      newDatas = {
+        Elementale_character: text,
+      };
+    } else if (name === 'celeste') {
+      newDatas = {
+        Celeste_character: text,
+      };
+    } else if (name === 'arcs') {
+      newDatas = {
+        Arcs_character: text,
+      };
+    } else if (name === 'tir') {
+      newDatas = {
+        Tir_character: text,
+      };
+    } else if (name === 'mainsNues') {
+      newDatas = {
+        MainsNues_character: text,
+      };
+    } else if (name === 'jets') {
+      newDatas = {
+        Jets_character: text,
+      };
+    } else if (name === 'hast') {
+      newDatas = {
+        ArmesHast_character: text,
+      };
+    } else if (name === 'tranchantes') {
+      newDatas = {
+        Tranchantes_character: text,
+      };
+    } else if (name === 'contondantes') {
+      newDatas = {
+        Contondantes_character: text,
+      };
+    } else if (name === 'esquive') {
+      newDatas = {
+        Esquive_character: text,
+      };
+    } else if (name === 'parade') {
+      newDatas = {
+        Parade_character: text,
+      };
+    } else if (name === 'magicotech') {
+      newDatas = {
+        MagicoTech_character: text,
+      };
+    } else if (name === 'carto') {
+      newDatas = {
+        Cartographie_character: text,
+      };
+    } else if (name === 'herbo') {
+      newDatas = {
+        Herboristerie_character: text,
+      };
+    } else if (name === 'medecine') {
+      newDatas = {
+        Medecine_character: text,
+      };
+    } else if (name === 'popo') {
+      newDatas = {
+        Potions_character: text,
+      };
+    } else if (name === 'theoMag') {
+      newDatas = {
+        TheorieMagique_character: text,
+      };
+    } else if (name === 'histoMag') {
+      newDatas = {
+        HistoireMagique_character: text,
+      };
+    } else if (name === 'air') {
+      newDatas = {
+        MagieAir_character: text,
+      };
+    } else if (name === 'eau') {
+      newDatas = {
+        MagieEau_character: text,
+      };
+    } else if (name === 'feu') {
+      newDatas = {
+        MagieFeu_character: text,
+      };
+    } else if (name === 'terre') {
+      newDatas = {
+        MagieTerre_character: text,
+      };
+    } else if (name === 'elec') {
+      newDatas = {
+        MagieElec_character: text,
+      };
+    } else if (name === 'crea') {
+      newDatas = {
+        Crea_character: text,
+      };
+    } else if (name === 'animaturgie') {
+      newDatas = {
+        Animaturgie_character: text,
+      };
+    } else if (name === 'vie') {
+      newDatas = {
+        MagieVie_character: text,
+      };
+    } else if (name === 'mort') {
+      newDatas = {
+        Mort_character: text,
+      };
+    } else if (name === 'temps') {
+      newDatas = {
+        Temps_character: text,
+      };
+    } else if (name === 'lumiere') {
+      newDatas = {
+        Lumiere_character: text,
+      };
+    } else if (name === 'tenebre') {
+      newDatas = {
+        Tenebres_character: text,
+      };
+    } else if (name === 'cosmos') {
+      newDatas = {
+        Cosmos_character: text,
+      };
+    } else if (name === 'invoc') {
+      newDatas = {
+        Invoc_character: text,
+      };
+    } else if (name === 'aura') {
+      newDatas = {
+        Aura_character: text,
+      };
+    } else if (name === 'magieAstrale') {
+      newDatas = {
+        MagieAstrale_character: text,
+      };
+    } else if (name === 'magieSpectrale') {
+      newDatas = {
+        MagieSpectrale_character: text,
+      };
+    } else if (name === 'magieDraconique') {
+      newDatas = {
+        MagieDraconique_character: text,
+      };
     };
-  } else if (name === 'dexte'){
-    newDatas = {
-      Dexte_character: text,
-    };
-  } else if (name === 'resistance'){
-    newDatas = {
-      Resistance_character: text,
-    };
-  } else if (name === 'resilience'){
-    newDatas = {
-      Resilience_character: text,
-    };
-  } else if (name ==='intell'){
-    newDatas = {
-      Intell_character: text,
-    };
-  } else if (name ==='charsime'){
-    newDatas = {
-      Charisme_character: text,
-    };
-  } else if (name ==='bien'){
-    newDatas = {
-      Bien_character: text,
-    };
-  } else if (name ==='mal'){
-    newDatas = {
-      Mal_character: text,
-    };
-  } else if (name ==='instinct'){
-    newDatas = {
-      Instinct_character: text,
-    };
-  } else if (name ==='survie'){
-    newDatas = {
-      Survie_character: text,
-    };
-  } else if (name ==='démonique'){
-    newDatas = {
-      Demonique_character: text,
-    };
-  } else if (name ==='draconique'){
-    newDatas = {
-      Draconique_character: text,
-    };
-  } else if (name ==='xalytien'){
-    newDatas = {
-      Xalytien_character: text,
-    };
-  } else if (name ==='xento'){
-    newDatas = {
-      Xento_character: text,
-    };
-  }else if (name ==='zenolm'){
-    newDatas = {
-      Zenolm_character: text,
-    };
-  } else if (name ==='justiccel'){
-    newDatas = {
-      Justiccel_character: text,
-    };
-  } else if (name ==='cerebrov'){
-    newDatas = {
-      Cerebrov_character: text,
-    };
-  } else if (name ==='xaArch'){
-    newDatas = {
-      XalytienArchaique_character: text,
-    };
-  } else if (name ==='xenArch'){
-    newDatas = {
-      XentoArchaique_character: text,
-    };
-  } else if (name ==='zenArch'){
-    newDatas = {
-      ZenolmArchaique_character: text,
-    };
-  } else if (name ==='justiArch'){
-    newDatas = {
-      JusticcelArchaique_character: text,
-    };
-  } else if (name ==='xaAnt'){
-    newDatas = {
-      XalytienAntique_character: text,
-    };
-  } else if (name ==='d4q'){
-    newDatas = {
-      XentoAntique_character: text,
-    };
-  } else if (name ==='xenAnt'){
-    newDatas = {
-      XentoAntique_character: text,
-    };
-  }else if (name ==='zenAnt'){
-    newDatas = {
-      ZenolmAntique_character: text,
-    };
-  } else if (name ==='justiAnt'){
-    newDatas = {
-      JusticcelAntique_character: text,
-    };
-  } else if (name ==='xaDem'){
-    newDatas = {
-      XalytienDemonique_character: text,
-    };
-  } else if (name ==='xenDem'){
-    newDatas = {
-      XentoDemonique_character: text,
-    };
-  } else if (name ==='ZenDem'){
-    newDatas = {
-      ZenolmDemonique_character: text,
-    };
-  }else if (name ==='JustiDem'){
-    newDatas = {
-      JusticcelDemonique_character: text,
-    };
-  } else if (name ==='zombik'){
-    newDatas = {
-      Zombik_character: text,
-    };
-  } else if (name ==='faerik'){
-    newDatas = {
-      Faerik_character: text,
-    };
-  }else if (name ==='elfik'){
-    newDatas = {
-      Elfik_character: text,
-    };
-  } else if (name ==='nanien'){
-    newDatas = {
-      Nanien_character: text,
-    };
-  } else if (name ==='gnomik'){
-    newDatas = {
-      Gnomik_character: text,
-    };
-  } else if (name ==='spectrale'){
-    newDatas = {
-      Spectrale_character: text,
-    };
-  } else if (name ==='astrale'){
-    newDatas = {
-      Astrale_character: text,
-    };
-  } else if (name ==='tenebriale'){
-    newDatas = {
-      Tenebriale_character: text,
-    };
-  } else if (name ==='noyale'){
-    newDatas = {
-      Noyale_character: text,
-    };
-  } else if (name ==='elementale'){
-    newDatas = {
-      Elementale_character: text,
-    };
-  }else if (name ==='celeste'){
-    newDatas = {
-      Celeste_character: text,
-    };
-  } else if (name ==='arcs'){
-    newDatas = {
-      Arcs_character: text,
-    };
-  } else if (name ==='tir'){
-    newDatas = {
-      Tir_character: text,
-    };
-  } else if (name ==='mainsNues'){
-    newDatas = {
-      MainsNues_character: text,
-    };
-  } else if (name ==='jets'){
-    newDatas = {
-      Jets_character: text,
-    };
-  } else if (name ==='hast'){
-    newDatas = {
-      ArmesHast_character: text,
-    };
-  } else if (name ==='tranchantes'){
-    newDatas = {
-      Tranchantes_character: text,
-    };
-  } else if (name ==='contondantes'){
-    newDatas = {
-      Contondantes_character: text,
-    };
-  } else if (name ==='esquive'){
-    newDatas = {
-      Esquive_character: text,
-    };
-  } else if (name ==='parade'){
-    newDatas = {
-      Parade_character: text,
-    };
-  } else if (name ==='magicotech'){
-    newDatas = {
-      MagicoTech_character: text,
-    };
-  } else if (name ==='carto'){
-    newDatas = {
-      Cartographie_character: text,
-    };
-  } else if (name ==='herbo'){
-    newDatas = {
-      Herboristerie_character: text,
-    };
-  } else if (name ==='medecine'){
-    newDatas = {
-      Medecine_character: text,
-    };
-  } else if (name ==='popo'){
-    newDatas = {
-      Potions_character: text,
-    };
-  } else if (name ==='theoMag'){
-    newDatas = {
-      TheorieMagique_character: text,
-    };
-  }else if (name ==='histoMag'){
-    newDatas = {
-      HistoireMagique_character: text,
-    };
-  } else if (name ==='air'){
-    newDatas = {
-      MagieAir_character: text,
-    };
-  } else if (name ==='eau'){
-    newDatas = {
-      MagieEau_character: text,
-    };
-  } else if (name ==='feu'){
-    newDatas = {
-      MagieFeu_character: text,
-    };
-  } else if (name ==='terre'){
-    newDatas = {
-      MagieTerre_character: text,
-    };
-  }  else if (name ==='elec'){
-    newDatas = {
-      MagieElec_character: text,
-    };
-  } else if (name ==='crea'){
-    newDatas = {
-      Crea_character: text,
-    };
-  } else if (name ==='animaturgie'){
-    newDatas = {
-      Animaturgie_character: text,
-    };
-  } else if (name ==='vie'){
-    newDatas = {
-      MagieVie_character: text,
-    };
-  } else if (name ==='mort'){
-    newDatas = {
-      Mort_character: text,
-    };
-  } else if (name ==='temps'){
-    newDatas = {
-      Temps_character: text,
-    };
-  } else if (name ==='lumiere'){
-    newDatas = {
-      Lumière_character: text,
-    };
-  } else if (name ==='tenebre'){
-    newDatas = {
-      Tenebres_character: text,
-    };
-  } else if (name ==='cosmos'){
-    newDatas = {
-      Cosmos_character: text,
-    };
-  } else if (name ==='invoc'){
-    newDatas = {
-      Invoc_character: text,
-    };
-  } else if (name ==='aura'){
-    newDatas = {
-      Aura_character: text,
-    };
-  } else if (name ==='magieAstrale'){
-    newDatas = {
-      MagieAstrale_character: text,
-    };
-  } else if (name ==='magieSpectrale'){
-    newDatas = {
-      MagieSpectrale_character: text,
-    };
-  } else if (name ==='magieDraconique'){
-    newDatas = {
-      MagieDraconique_character: text,
-    };
-  };
     console.log(newDatas);
 
 
 
-const value = Number(text);
-if (!Number.isFinite(value)) {
-  alert("Valeur invalide");
-  return;
-}
+    const value = Number(text);
+    if (!Number.isFinite(value)) {
+      alert("Valeur invalide");
+      return;
+    }
 
-fetch(`/api/characters/updateCharacter/${character.character.ID_character}`, {
-  method: "PUT",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(newDatas),
-})
-  .then(async (res) => {
-    const body = await res.json().catch(() => null);
-    if (!res.ok) throw new Error(`HTTP ${res.status}: ${JSON.stringify(body)}`);
-    return body;
-  })
-  .then((updated) => {
-    console.log("Success:", updated);
-    setOpen(false);
-  })
-  .catch((err) => console.error("Error:", err));
+    fetch(`/api/characters/updateCharacter/${character.character.ID_character}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newDatas),
+    })
+      .then(async (res) => {
+        const body = await res.json().catch(() => null);
+        if (!res.ok) throw new Error(`HTTP ${res.status}: ${JSON.stringify(body)}`);
+        return body;
+      })
+      .then((updated) => {
+        console.log("Success:", updated);
+        setOpen(false);
+      })
+      .catch((err) => console.error("Error:", err));
 
   };
 
   return (
-    <div style={{position: 'relative', height:'0px'}}>
-      <Button sx={{ position:'relative', display: 'flex', left:{left}, top:'-2.3vh', border:'none'}} variant="outlined" onClick={handleClickOpen}>
-        <img src={Pencil} color={theme.custom.mycustomblur.text}  height={'15px'} alt="Modifier" id='modifier'/>
+    <div style={{ position: 'relative', height: '0px' }}>
+      <Button sx={{ position: 'relative', display: 'flex', left: { left }, top: '-2.3vh', border: 'none' }} variant="outlined" onClick={handleClickOpen}>
+        <img src={Pencil} color={theme.custom.mycustomblur.text} height={'15px'} alt="Modifier" id='modifier' />
       </Button>
       <Dialog
         onClose={handleClose}
         open={open}
-        PaperProps={{style:{minWidth:'30vw', minHeight:'30vh', backgroundColor:'beige'},}}
+        PaperProps={{ style: { minWidth: '30vw', minHeight: '30vh', backgroundColor: 'beige' }, }}
       >
-        <DialogTitle onClose={handleClose} sx={{outerWidth:'50vw'}}>
+        <DialogTitle onClose={handleClose} sx={{ outerWidth: '50vw' }}>
           Entrez la nouvelle valeur de {dataToUpdate}
         </DialogTitle>
         <DialogContent>
 
-            <div>
-              <TextField id="newNotes" multiline sx={{minHeight:'30px', minWidth:'30px'}} onChange={(e) => setText(e.target.value)} inputProps={{style:{ height:"150px"}}}/>
-              <Button onClick={() => {
-                handleSave();
-              }
-              }> Save new value</Button>
-            </div> 
+          <div>
+            <TextField id="newNotes" multiline sx={{ minHeight: '30px', minWidth: '30px' }} onChange={(e) => setText(e.target.value)} inputProps={{ style: { height: "150px" } }} />
+            <Button onClick={() => {
+              handleSave();
+            }
+            }> Save new value</Button>
+          </div>
         </DialogContent>
         <DialogActions>
         </DialogActions>
