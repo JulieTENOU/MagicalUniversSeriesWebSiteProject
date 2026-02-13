4ffd77d742c784e2eb5459b0b11fa878d3837711
Julie TENOU <julie.tenou@epitech.eu>
2026-02-13 22:55:19 +0000

    fix: update return button for better sizing

--- front/src/components/BtnRtn.jsx
+++ front/src/components/BtnRtn.jsx
@@ -1,19 +1,34 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import LogoReturn from "../assets/img/return.png";
+import { useTheme } from "@mui/material/styles";
 
 export default function BtnRtn(props) {
-    const navigate= useNavigate();
-    return (
-        <div style={props.style}>
-            {/* <Link to={props.path}> */}
-                <Button sx={props.sx} onClick={() => navigate(-1)}>
-                    <div style={{ display: "flex", flexDirection: "column", justifyContent:"center", alignItems:'center'}}>
-                       <img src={LogoReturn} alt="Retour" height="10vh" width="20vw" />
-           {props.msg}
-                    </div>
-                </Button>
-            {/* </Link> */}
+  const theme = useTheme();
+  const navigate = useNavigate();
+  return (
+    <div style={props.style}>
+      {/* <Link to={props.path}> */}
+      <Button sx={props.sx} onClick={() => navigate(-1)}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={LogoReturn}
+            alt="Retour"
+            style={{ height: "5vh", width: "7vw" }}
+          />
+          <Typography sx={{ color: theme.custom.mycustomblur.text }}>
+            {props.msg}
+          </Typography>
         </div>
-    )
-};
+      </Button>
+      {/* </Link> */}
+    </div>
+  );
+}
